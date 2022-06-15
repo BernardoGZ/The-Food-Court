c56384910d5350b392b6876164118ae68b70be8f
BernardoG <A00570682@itesm.mx>
2022-06-15 16:50:40 -0500

    Added new screens and activities

--- TheFoodCourt/app/src/main/java/com/a00570682/thefoodcourt/RestaurantMenu.kt
+++ TheFoodCourt/app/src/main/java/com/a00570682/thefoodcourt/RestaurantMenu.kt
@@ -2,14 +2,14 @@ package com.a00570682.thefoodcourt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.a00570682.thefoodcourt.databinding.ChilaquilesTecScreenBinding
+import com.a00570682.thefoodcourt.databinding.RestaurantChilaquilesBinding
 
 class RestaurantMenu : AppCompatActivity() {
-    private lateinit var binding: ChilaquilesTecScreenBinding
+    private lateinit var binding: RestaurantChilaquilesBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ChilaquilesTecScreenBinding.inflate(layoutInflater)
+        binding = RestaurantChilaquilesBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
